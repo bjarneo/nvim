1a0511ae8b851189b3e3fbe3efacd720fabe52a0
bjarneo <bjarne.oeverli@gmail.com>
2024-09-17 18:44:15 +0200

    feat: add chunk arrow

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -7,7 +7,19 @@ return {
     "shellRaining/hlchunk.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("hlchunk").setup({})
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          chars = {
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "╭",
+            left_bottom = "╰",
+            right_arrow = ">",
+          },
+          style = "#806d9c",
+        },
+      })
     end,
   },
 }
